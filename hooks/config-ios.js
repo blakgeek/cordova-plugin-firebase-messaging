921507dae3daa6f851d4bbf72444f329a7ba5fff
blakgeek <blakgeek@gmail.com>
2019-07-04 12:18:06 -0400

    Fixed require statements and removed the use of Q for promises

--- hooks/config-ios.js
+++ hooks/config-ios.js
@@ -4,55 +4,50 @@ var xcode = require('xcode');
 var plist = require('plist');
 var shell = require('shelljs');
 var path = require('path');
+var semver = require('semver');
 
 
 module.exports = function (context) {
 
-    //return;
-
     // only execute for android
     if (!forPlatform('ios')) return;
 
-    var Q = context.requireCordovaModule('q');
-    var deferred = Q.defer();
     var rootPath = context.opts.projectRoot;
     var appName = getConfigParser(context, 'config.xml').name();
     var iosConfigSrc = path.join(rootPath, 'GoogleService-Info.plist');
     var iosConfigDestDir = path.join(rootPath, 'platforms', 'ios', appName, '/Resources');
-    var appPlistPath = path.join(rootPath,  'platforms', 'ios', appName, appName + '-Info.plist');
+    var appPlistPath = path.join(rootPath, 'platforms', 'ios', appName, appName + '-Info.plist');
     var iosConfigDest = path.join(iosConfigDestDir, 'GoogleService-Info.plist');
-    var projectPath = path.join(rootPath,  'platforms', 'ios', appName + '.xcodeproj/project.pbxproj');
+    var projectPath = path.join(rootPath, 'platforms', 'ios', appName + '.xcodeproj/project.pbxproj');
     var iosConfigExists = fs.existsSync(iosConfigSrc);
     var project = xcode.project(projectPath);
 
     if (iosConfigExists && filesDiffer(iosConfigSrc, iosConfigDest)) {
 
-        console.log('Copying GoogleService-Info.plist');
-        shell.mkdir('-p', iosConfigDestDir);
-        shell.cp(iosConfigSrc, iosConfigDest);
+        return new Promise((resolve) => {
+            console.log('Copying GoogleService-Info.plist');
+            shell.mkdir('-p', iosConfigDestDir);
+            shell.cp(iosConfigSrc, iosConfigDest);
 
-        var configPlist = plist.parse(fs.readFileSync(iosConfigSrc, 'utf8'));
-        var appPlist = plist.parse(fs.readFileSync(appPlistPath, 'utf8'));
-        appPlist.CFBundleURLTypes = (appPlist.CFBundleURLTypes || []).concat([
-            {
-                "CFBundleTypeRole": "Editor",
-                "CFBundleURLSchemes": [configPlist.REVERSED_CLIENT_ID]
-            }
-        ]);
-        fs.writeFileSync(appPlistPath, plist.build(appPlist));
+            var configPlist = plist.parse(fs.readFileSync(iosConfigSrc, 'utf8'));
+            var appPlist = plist.parse(fs.readFileSync(appPlistPath, 'utf8'));
+            appPlist.CFBundleURLTypes = (appPlist.CFBundleURLTypes || []).concat([
+                {
+                    "CFBundleTypeRole": "Editor",
+                    "CFBundleURLSchemes": [configPlist.REVERSED_CLIENT_ID]
+                }
+            ]);
+            fs.writeFileSync(appPlistPath, plist.build(appPlist));
 
-        project.parse(function (err) {
+            project.parse(function (err) {
 
-            console.log('adding resource');
-            project.addResourceFile('GoogleService-Info.plist');
-            fs.writeFileSync(projectPath, project.writeSync());
-            deferred.resolve();
+                console.log('adding resource');
+                project.addResourceFile('GoogleService-Info.plist');
+                fs.writeFileSync(projectPath, project.writeSync());
+                resolve();
+            });
         });
-    } else {
-        deferred.resolve();
     }
-
-    return deferred.promise;
 
 
     function forPlatform(platform) {
@@ -75,7 +70,6 @@ module.exports = function (context) {
     }
 
     function getConfigParser(context, config) {
-        var semver = context.requireCordovaModule('semver');
         var ConfigParser;
 
         if (semver.lt(context.opts.cordova.version, '5.4.0')) {
